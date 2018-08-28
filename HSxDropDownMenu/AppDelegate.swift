adaee8fcc7f55bd126493988e21bbd7fa6730460
mily77 <773258709@qq.com>
2018-08-28 17:38:32 +0800

    add MianVC

--- HSxDropDownMenu/AppDelegate.swift
+++ HSxDropDownMenu/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let mainVC = MainViewController.init()
+        let nav = UINavigationController.init(rootViewController: mainVC)
+        self.window?.rootViewController = nav
+        
         return true
     }
 
